1576bd1140c8d9faba34be785bdddad1d10503a8
Doug Brauch <doug.c.brauch@outlook.com>
2019-02-20 10:13:13 -0500

    Add project files.

---
+++ MathFunctions/MathFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MathFunctions
+{
+    public class MathFunctionsClass
+    {
+        //Square:: Double ->
+        public double Square(double value)
+        {
+            return value * value;
+        }
+        public double Cube(double value)
+        {
+            return Math.Pow(value, 3);
+        }
+    }
+}
